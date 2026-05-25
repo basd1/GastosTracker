53f31952cd4d2b450e6741b1c01f865e31188c8d
basd1 <bastianorellana1106@gmail.com>
2026-05-25 20:08:40 +0200

    chore: add imports for tap interaction on savings chart

--- presentation/src/main/java/bas/orellana/gastostracker/presentation/ui/screen/GraphScreen.kt
+++ presentation/src/main/java/bas/orellana/gastostracker/presentation/ui/screen/GraphScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -41,6 +42,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight

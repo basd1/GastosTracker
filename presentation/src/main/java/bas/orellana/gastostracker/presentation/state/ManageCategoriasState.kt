47dd677c37374af9510223b3186393e71e587573
basd1 <bastianorellana1106@gmail.com>
2026-05-28 00:26:51 +0200

    [3.4] Add edit fields to ManageCategoriasState

--- presentation/src/main/java/bas/orellana/gastostracker/presentation/state/ManageCategoriasState.kt
+++ presentation/src/main/java/bas/orellana/gastostracker/presentation/state/ManageCategoriasState.kt
@@ -1,13 +1,20 @@
 package bas.orellana.gastostracker.presentation.state
 
 import bas.orellana.gastostracker.domain.model.Categoria
+import bas.orellana.gastostracker.domain.model.CategoriaPersonalizada
 
 data class ManageCategoriasState(
     val nuevaCategoria: String = "",
-    val colorSeleccionado: Long = Categoria.PALETA_COLORES_PERSONALIZADOS.first()
+    val colorSeleccionado: Long = Categoria.PALETA_COLORES_PERSONALIZADOS.first(),
+    val editCategoria: CategoriaPersonalizada? = null,
+    val editNombre: String = "",
+    val editColor: Long = Categoria.PALETA_COLORES_PERSONALIZADOS.first()
 ) {
     fun reset() = copy(
         nuevaCategoria = "",
-        colorSeleccionado = Categoria.PALETA_COLORES_PERSONALIZADOS.first()
+        colorSeleccionado = Categoria.PALETA_COLORES_PERSONALIZADOS.first(),
+        editCategoria = null,
+        editNombre = "",
+        editColor = Categoria.PALETA_COLORES_PERSONALIZADOS.first()
     )
 }
